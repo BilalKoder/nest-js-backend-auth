331426042b0a70d218ec643b229b21be7e100378
BilalFarnek <bilal.ali@farnek.com>
2025-01-19 13:50:31 +0400

    Removed User Decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { User } from './auth/decorators/user.decorator';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { Public } from './auth/decorators/public.decorator';
